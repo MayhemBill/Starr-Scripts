aa952be9d8f35fb5c17014c627b0c69c18c4fb55
MayhemBill <theherobilly@gmail.com>
2023-10-22 09:36:47 -0400

    Removed testing scripts

--- misc/nitsua.py
+++
@@ -1,7 +0,0 @@
-import json
-import subprocess
-
-NOTIFIARR_PY = '/mnt/development/scripts/misc/notifiarr.py'
-
-webhook = "python3 " + NOTIFIARR_PY + " -e \"Errors\" -c 1164585523348783104 -m \"FFA500\" -t \"Error\" -b \"URLs found with errors\" -a \"https://notifiarr.com/images/logo/notifiarr.png\" -z \"Passthrough Integration\""
-subprocess.call(webhook)
--- misc/path.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-p = Path(__file__).parents[1]
-
-print(p)
